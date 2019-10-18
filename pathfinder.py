8966676cfb5d3007bd7bf0c4434a9ffdb5a1a6ee
Ryan <rydowd@gmail.com>
2019-10-18 16:35:58 -0400

    Implemented multiprocessing and an algorithm to allow vertical movement

--- pathfinder.py
+++ pathfinder.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import random as r
+from multiprocessing import Pool
 
 class Path:
   """
@@ -7,19 +8,21 @@ class Path:
   list of tuples, containing the coordinates that it visits on its
   way across the map, and tracks its elevation change step to step.
   """
-  def __init__(self, MapData, position = (0,0)):
+  def __init__(self, MapData, position, max_x, max_y):
     self.coordinates = MapData.coordinates
     self.path = [position]
     self.elevation_change = []
     self.position = position
-    self.max_x = max(self.coordinates)[0]
-    self.max_y = max(self.coordinates)[1]
-    print(self.position)
+    self.elevation = self.coordinates[self.position]
+    self.max_x = max_x
+    self.max_y = max_y
     self.walk()
     
-  # def valid_steps(self):
-  #   current_x = self.position[0] 
-  #   current_y = self.position[1]    
+  def valid_steps(self):
+    current_x = self.position[0] 
+    current_y = self.position[1]
+    valid = [(x,y)for x in range(current_x, current_x +2) for y in range(current_y - 1, current_y + 2) if (0<= x <= self.max_x) & (0<= y <= self.max_y)]
+    return [option for option in valid if option not in self.path[-2:]]
     
   def valid_forward_steps(self):
     current_x = self.position[0] 
@@ -27,31 +30,26 @@ class Path:
     
     return[(current_x+1, choice) for choice in range(current_y-1, current_y+2)if (0 <= choice <= self.max_y)]
    
-  def choose_step(self, valid_steps):
+  def choose_step(self, options):
     current_elevation = self.coordinates[self.position]
-    options_dict = {step:(abs(current_elevation-self.coordinates[step])+r.random()) for step in valid_steps}
+    options_dict = {step:(abs(current_elevation-self.coordinates[step])+r.random()) for step in options}
     choice = min(options_dict.items(), key = lambda x:x[1])
     self.elevation_change.append(int(choice[1]))
+    self.elevation = self.coordinates[choice[0]]
     return choice[0]
     
   def take_step(self):
-    valid_steps = self.valid_forward_steps()
-    self.position = self.choose_step(valid_steps)
+    options = self.valid_steps()
+    self.position = self.choose_step(options)
     self.path.append(self.position)
-    
-    
     
   def walk(self):
     while self.position[0] < self.max_x:
       self.take_step()
     else:
       self.total_elevation_change = sum(self.elevation_change)
-      print(self.total_elevation_change)
-      # print(self.path)
   
   
-    
-
 class MapData:
   """
   Class that holds onto information about our map and draws on it.
@@ -84,29 +82,27 @@ class MapData:
     return (greyscale, greyscale, greyscale, 255)
   
   def draw_topo_map(self):
-    size = max(self.coordinates)
-    print(size)
-    self.canvas = Image.new('RGBA',(size[0]+1, size[1]+1))
+    self.size = max(self.coordinates)
+    print(self.size)
+    self.canvas = Image.new('RGBA',(self.size[0]+1, self.size[1]+1))
     for item in self.coordinates.items():
       self.canvas.putpixel(item[0], self.get_color(item[1]))
     self.canvas.save('test.png')
     
-  def forge_random_path(self):
-    y_start = r.randrange(max(self.coordinates)[1]+1)
-    self.paths.append(Path(self, (0, y_start)))
-    
   def forge_all_paths(self):
-    for y in range(max(self.coordinates)[1]+1):
-      self.paths.append(Path(self, (0, y)))
-    
-    
+    pool = Pool(processes = 4)
+    coords = [(0,y) for y in range(max(self.coordinates)[1]+1)]
+    self.paths = pool.map(self.forge_path, coords)
+  
+  def forge_path(self, start):
+    return Path(self, start, self.size[0], self.size[1])
+      
   def draw_paths(self):
     for path in self.paths:
       for location in path.path:
         self.path_set.add(location)
     for location in self.path_set:
       self.canvas.putpixel(location, (255,0,0,255))
-      # print('painting')
     self.canvas.save('test.png')
     
   def draw_best_path(self):
@@ -117,8 +113,9 @@ class MapData:
       self.canvas.putpixel(location, (0,255,0,255))
     self.canvas.save('test.png')
 
+
 if __name__ == "__main__":
-  new_map = MapData('elevation_large.txt')
+  new_map = MapData('elevation_small.txt')
   new_map.assign_coordinates(new_map.data)
   new_map.draw_topo_map()
   # new_map.forge_random_path()
